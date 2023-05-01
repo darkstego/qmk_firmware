650b034a0b068b3414a7fabd8358a08816642865
Abdulla Bubshait <darkstego@gmail.com>
2023-05-01 14:41:14 -0400

    Change ESC shortcut from single click ALT to double click

--- keyboards/keychron/q5/ansi_encoder/keymaps/wakib/keymap.c
+++ keyboards/keychron/q5/ansi_encoder/keymaps/wakib/keymap.c
@@ -29,6 +29,15 @@ enum wakib_keycodes {
     KC_WAKIB_TOGGLE = SAFE_RANGE,
 };
 
+enum {
+    TD_ALT_ESC,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ALT_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_ESC),
+};
 
 // List of Wakib Shortcuts
 // All Shortcuts that use the shift modifier appear at the end of the list
@@ -72,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGUP,    KC_HOME,    KC_END,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_PGDN,    KC_UP,    KC_PAUS,    KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,    KC_LEFT,    KC_DOWN,    KC_RIGHT,
-        KC_LCTL,  KC_LWIN,  LALT_T(KC_ESC),                            KC_SPC,                                 KC_RALT,  MO(L_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT,  KC_PENT),
+        KC_LCTL,  KC_LWIN,  TD(TD_ALT_ESC),                            KC_SPC,                                 KC_RALT,  MO(L_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT,  KC_PENT),
     [L_NUMLOCK] = LAYOUT_ansi_98(
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,  _______,
